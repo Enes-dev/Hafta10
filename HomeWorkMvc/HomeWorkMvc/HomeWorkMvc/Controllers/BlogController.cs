6c440a17b77c7696c570c3163400c400afc1f036
Enes Demir <enes456746@gmail.com>
2024-10-22 21:55:56 +0300

    Güncel

---
+++ HomeWorkMvc/HomeWorkMvc/HomeWorkMvc/Controllers/BlogController.cs
@@ -0,0 +1,27 @@
+﻿using BlogApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlogApp.Controllers
+{
+    public class BlogController : Controller
+    {
+        public IActionResult Index()
+        {
+            Blog blog1 = new Blog(1,"başlık","açıklama","Siteye Hoşgeldiniz", "https://picsum.photos/id/237/200/300");
+            Blog blog2 = new Blog(1,"başlık1","açıklama1","C#", "https://picsum.photos/id/237/200/300");
+            Blog blog3 = new Blog(1,"başlık2","açıklama2","HTML&CSS", "https://picsum.photos/id/237/200/300");
+            Blog blog4 = new Blog(1,"başlık3","açıklama3","JavaScript", "https://picsum.photos/id/237/200/300");
+            Blog blog5 = new Blog(1,"başlık4","açıklama4","Python", "https://picsum.photos/id/237/200/300");
+
+            List<Blog> list = new List<Blog>();
+            list.Add(blog1);
+            list.Add(blog2);
+            list.Add(blog3);
+            list.Add(blog4);
+            list.Add(blog5);
+
+
+            return View(list);
+        }
+    }
+}
